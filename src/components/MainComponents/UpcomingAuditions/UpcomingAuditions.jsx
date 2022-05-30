e36a66fa697b404b30eda177d1d2e8c513b0b961
Jake McInerney <jakemcinerney1@gmail.com>
2022-05-30 16:11:42 -0500

    added the SearchBar component to PastAuditions and UpcomingAuditions

--- src/components/MainComponents/UpcomingAuditions/UpcomingAuditions.jsx
+++ src/components/MainComponents/UpcomingAuditions/UpcomingAuditions.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import AuditionItem from '../AuditionItem/AuditionItem';
+import SearchBar from '../SearchBar/SearchBar';
 import './UpcomingAuditions.css';
 
 function UpcomingAuditions() {
@@ -16,6 +17,9 @@ function UpcomingAuditions() {
   return (
     <>
       <h2 className='upcoming-h2'>Upcoming Auditions</h2>
+      <div className='upcoming-search-bar'>
+        <SearchBar placeholder='Enter Audition...' data={auditions} />
+      </div>
       {auditions.map((audition) => {
         if (audition.audition_complete === false) {
           return <AuditionItem key={audition.id} audition={audition} />;
